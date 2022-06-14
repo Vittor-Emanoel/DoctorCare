2ef462d153ac0c7b0c38bde20ed478ce671e8480
Vittor-Emanoel <55211291+Vittor-Emanoel@users.noreply.github.com>
2022-06-13 22:14:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+function onScroll() {
+  showNaveOnScroll()
+  showBackToTopButtonOn ()
+}
+function showNaveOnScroll() {
+  if (scrollY > 0) {
+    const nav = document.getElementById("navigation").classList;
+    nav.add("scroll");
+  } else {
+    const nav = document.getElementById("navigation").classList;
+    nav.remove("scroll");
+  }
+}
+function showBackToTopButtonOn (){
+  if (scrollY > 1400) {
+    const nav = document.getElementById("backToTopButton").classList;
+    nav.add("show");
+  } else {
+    const nav = document.getElementById("backToTopButton").classList;
+    nav.remove("show");
+  }
+}
+
+function openMenu() {
+   const menu = document.body.classList.add('menu-expanded');
+}
+function closeMenu() {
+    const close = document.body.classList.remove('menu-expanded');
+}
+
+//Animations
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+}).reveal(`
+  #home,
+  #home img,
+  #home 
+  .stats,
+  #services,
+  #services header,
+  #services .card,
+  #about,
+  #about header,
+  #about .content`); /*Template strings */
